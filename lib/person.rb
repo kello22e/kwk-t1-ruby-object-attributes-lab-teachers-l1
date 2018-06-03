b3a6618f737238dd1c8cd295290afe09f3d7d73a
kello22e <kello22e@mtholyoke.edu>
2018-06-03 18:54:48 +0000

    automatically backed up by learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person 
+  def name=(person_name)
+    @this_person_name = person_name
+  end
+  def name 
+    @this_person_name
+  end
+  def job=(person_job)
+    @this_person_job = dog_breed
+  end
+  def breed
+    @this_dog_breed
+  end
+end
